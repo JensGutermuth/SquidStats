7c86d81127939768ccd2f396d6fcde9a688914dc
Jens Gutermuth <jens@gutermuth-online.de>
2010-08-21 22:58:57 +0200

    simple setup-routine

---
+++ web/setup.php
@@ -0,0 +1,7 @@
+<?
+    require_once("../helper/ConfigHandler.class.php");
+    require_once("../helper/LogHandler.class.php");
+    require_once("../helper/TemplateHandler.class.php");
+    LogHandler::setup();
+    ConfigHandler::setup();
+?>
