daf93128bc142d874e7054e7a25e4ad4bb3249af
unknown <admin@.(none)>
2010-08-22 17:50:18 +0200

    bereitet aktuell Probleme mit fopen... Verbesserung der effizienz, speichern in die datei vervollständigt.

--- helper/ConfigHandler.class.php
+++ helper/ConfigHandler.class.php
@@ -4,28 +4,43 @@
  * @Description: Verwaltung von Einstellungen & Eigenschaften
  * @Version: 0.1
  **/
-
+ 
 require_once("DbHandler.class.php");
 require_once("../config.php");
 
 	class ConfigHandler implements arrayaccess
 	{
 		private $db;
-        static private $instance;
+		static private $instance;
 		private $property = array();
-        private $property_ready = false;
+		private $property_file = array();
+		private $property_changed = array();
+		private $property_ready = false;
+		private $any_filechanges = false;
 		
-        public static function getInstance()
-        {
-            if(!self::$instance) {
-                self::$instance = new ConfigHandler();
-            }
-            return self::$instance;            
-        }
-        
-        private function loadPropertiesFromDb() {
-            $this->property_ready = true;
-            $this->db = Dbhandler::getInstance();
+		public static function getInstance()
+		{
+			if(!self::$instance) {
+				self::$instance = new ConfigHandler();
+			}
+			return self::$instance;			
+		}
+		
+		public function setup() 
+		{
+			$db = DbHandler::getInstance();
+			$sql = "CREATE TABLE `property` (
+					`name` VARCHAR( 255 ) NOT NULL ,
+					`val` TEXT NOT NULL ,
+					PRIMARY KEY ( `name` )
+				) ENGINE = MYISAM ;";
+			$db->query($sql);
+		}
+		
+		private function loadPropertiesFromDb() 
+		{
+			$this->property_ready = true;
+			$this->db = Dbhandler::getInstance();
 			
 			$result = $this->db->query("SELECT * FROM `property`;");
 			if(!$result)
@@ -38,102 +53,163 @@ require_once("../config.php");
 				}
 				$result->free();
 			}
-        }
-        
-		private function __construct() 
-		{
-            // ToDo
-            $this->property = get_config_from_config_file();
 		}
-        
-        public function setup() {
-            $db = DbHandler::getInstance();
-            $sql = "CREATE TABLE `property` (
-                `name` VARCHAR( 255 ) NOT NULL ,
-                `val` TEXT NOT NULL ,
-                PRIMARY KEY ( `name` )
-                ) ENGINE = MYISAM ;";
-            $db->query($sql);
-        }
 		
-        public function get($name)
-        {
-//            echo "bla ".var_dump($this)."\n";
+		private function __construct() 
+		{ 
+			ini_set("error_reporting", E_ALL | E_STRICT);
+			//get_config_from_config_file();
+		}
+		
+		public function __destruct()
+		{
+			$this->write_config();
+		}
+		
+		private function write_config()
+		{
+			$file_data = array();
+			foreach($this->property_changed as $key => $data)
+			{
+				if(!isset($this->property_file[$key]))
+				{
+					$val = serialize($val);
+					$result = $this->db->query("INSERT INTO `property` SET 
+													val = '".$this->db->escape_string($val)."',
+													name = '".$this->db->escape_string($name)."' 
+												ON DUPLICATE KEY
+													UPDATE `property` SET 
+													val = '".$this->db->escape_string($val)."',
+													name = '".$this->db->escape_string($name)."';");
+					if($result === false)
+						throw new Exception("MySQL Fehler");
+					$result->free();
+				}
+			}
+			if($this->any_filechanges == true)
+				$this->writeFileConf();
+		}
+		
+		public function get($name)
+		{
 			if(isset($this->property[$name]))
 				return $this->property[$name];
 			else
-                if (!$this->property_ready) {
-                    $this->loadPropertiesFromDb();
-                    return $this->get($name);
-                } else {
-                    throw new Exception("Ungueltiger Name ".$name);
-                }
-        }
+				if (!$this->property_ready) {
+					$this->loadPropertiesFromDb();
+					return $this->get($name);
+				} else {
+					throw new Exception("Ungueltiger Name ".$name);
+				}
+		}
 
-        public function set($name, $val)
-        {
-            if (!$this->property_ready) {
-                $this->loadPropertiesFromDb();
-            }
-            $this->property[$name] = $val;
-   			$val = serialize($val);
-			$result = $this->db->query("INSERT INTO `property` SET 
-									val = '".$this->db->escape_string($val)."',
-									name = '".$this->db->escape_string($name)."' 
-								ON DUPLICATE KEY
-									UPDATE `property` SET 
-									val = '".$this->db->escape_string($val)."',
-									name = '".$this->db->escape_string($name)."';");
-			if($result === false)
-				throw new Exception("MySQL Fehler");
-			$result->free();
-        }
-        
-        public function exists($name)
-        {
-            if (!$this->property_ready) {
-                $this->loadPropertiesFromDb();
-            }
-            return isset($this->property[$name]);
-        }
-        
-        // Zugriff über Eigenschaften
+		public function set($name, $val)
+		{
+			$this->property[$name] = $val;
+			
+			if(isset($this->property_file[$name]))
+				$this->setFileConf($name, $name);
+			else 
+			{
+				$val = serialize($val);
+				$result = $this->db->query("INSERT INTO `property` SET 
+												val = '".$this->db->escape_string($val)."',
+												name = '".$this->db->escape_string($name)."' 
+											ON DUPLICATE KEY
+												UPDATE `property` SET 
+												val = '".$this->db->escape_string($val)."',
+												name = '".$this->db->escape_string($name)."';");
+				if($result === false)
+					throw new Exception("MySQL Fehler");
+				$result->free();
+			}
+		}
+		
+		public function exists($name)
+		{
+			if (!$this->property_ready) {
+				$this->loadPropertiesFromDb();
+			}
+			return isset($this->property[$name]);
+		}
+		
+		// Zugriff über Eigenschaften
 		public function __get($name)
 		{
-            $this->get($name);
+			return $this->get($name);
 		}
 		
 		public function __set($name, $val)
 		{
-            $this->set($name, $val);
+			$this->set($name, $val);
 		}
 		
 		public function __isset($name)
 		{
-            $this->exists($name);
+			return $this->exists($name);
 		}
 
-        // arrayaccess
-        
-        public function offsetExists ($name)
-        {
-            return $this->exists($name);
-        }
-            
-        public function offsetGet($name)
-        {
-            return $this->get($name);
-        }
-            
-        public function offsetSet($name, $val)
-        {
-            $this->set($name, $val);
-        }
-            
-        public function offsetUnset($name)
-        {
-            // ToDo
-        }
-            		
+		// arrayaccess
+		
+		public function offsetExists ($name)
+		{
+			return $this->exists($name);
+		}
+			
+		public function offsetGet($name)
+		{
+			return $this->get($name);
+		}	
+			
+		public function offsetSet($name, $val)
+		{
+			$this->set($name, $val);
+		}
+			
+		public function offsetUnset($name)
+		{
+			// ToDo
+		}
+		
+		
+		
+		// Datei-funktionen
+		// 
+		// LANGSAM! nur für einstellungen, die nicht häufig geändert werden müssen!
+		
+		private function writeFileConf()
+		{
+			$file = fopen("config.php", "w+", 1);
+			fputs($file, "<?php \n".
+						 "function get_config_from_config_file() {".
+						 "\$config = ConfigHandler::getInstance();\n");
+			
+			foreach($this->property_file as $key=> &$data)
+			{
+				fputs($file, '$config->setFileconf('.$key.', '.var_export($this->property[$key], true).', 0); '."\n");
+			}
+			
+			fputs($file, "} ?>");
+			fclose($file);
+		}
+		
+		public function setFileConf($name, $val, $new = 1)
+		{
+			$this->property[$name] = $val;
+			$this->property_file[$name] = "y"; // der array-index muss existieren...
+			if($new == 1)
+				$this->any_filechanges = true;
+		}
+		
+		public function delFileConf($name)
+		{
+			unset($this->property[$name]);
+			unset($this->property_file[$name]);
+			$this->any_filechanges = true;
+		}		
 	}
+	
+	$conf = ConfigHandler::getInstance();
+	$conf->setFileConf("bla", "f00bar1", 1);
+	$conf->setFileConf("bla1", "f00bar2", 1);
 ?>
