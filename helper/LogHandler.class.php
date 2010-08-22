1ec1beb6de9837afcfc97d46f6126630eeb4587c
Jens Gutermuth <jens@gutermuth-online.de>
2010-08-22 21:41:46 +0200

    LogHandler schöner gemacht
    1. Mehrmals benutzte Blöcke als Funktion ausgegliedert (formatOrigin)
    2. bei Logs von DB-Querys weitere Informationen aus dem backtrace hinzugefügt

--- helper/LogHandler.class.php
+++ helper/LogHandler.class.php
@@ -28,28 +28,48 @@ class LogHandler {
     private static $enabled = true;
     // Um nicht selbst Log-Einträge zu erzeugen und damit in endloser Rekursion zu enden
     
+    private function formatOrigin($trace, $root) {
+        // Dateiname von der Position des Projekts befreien
+        $basename = realpath(dirname(__FILE__).'/..');
+        $file = $trace[$root]['file']; // Code wird kürzer
+        $trace[$root]['file'] = substr($file, strlen($basename)+1, strlen($file));
+        $tmp = $trace[$root]['file'].' ('.$trace[$root]['line'].'): ';
+        if (isset($trace[$root+1])) {
+            if (isset($trace[$root+1]['class'])) { // Klassenfunktion
+                $tmp .= $trace[$root+1]['class'].$trace[$root+1]['type'];
+            }
+            $tmp .= $trace[$root+1]['function'].": ";
+        }
+        $tmp .= "\n";
+        return $tmp;
+    }
+    
     public function log($severity, $message) {
         $config = ConfigHandler::getInstance();
         if (($severity > $config->log['min_severity']) and (Loghandler::$enabled)) {
             Loghandler::$enabled = false;
             $trace = debug_backtrace(false);
             $origin = "";
-            if (isset($trace[0])) {
-                $caller = $trace[0];
-                $origin = $caller["file"]." (".$caller["line"]."): ";
-//                $origin = "";
-                if (count($trace) > 1) { // Wir wurden aus einer Funktion heraus aufgerufen
-                    $subcaller = $trace[1];
-                    if (isset($subcaller['class'])) { // Klassenfunktion
-                        $origin .= $subcaller['class'].$subcaller['type'];
+            if (count($trace) > 1) {
+                if (($severity == LogHandler::SEVERITY_DB_QUERY) &&
+                ($trace[1]['function'] == '__call')) { // Speziallfall für DB-Querys
+                    if (count($trace) > 2) {
+                        $message = 'Query von '.$this->formatOrigin($trace, 2).$message;
                     }
-                    $origin .= $subcaller['function'];
+                    // __call verstecken, und die "richtige" Funktion ausgeben
+                    $trace[1]['function'] = $trace[1]['args'][0];
                 }
             }
+            if (isset($trace[0])) {
+                $origin = $this->formatOrigin($trace, 0);
+            }
             $db = DbHandler::getInstance();
-
+            
             // Doppelte Leerzeichen entfernen, sind in 99,9% der Fälle Schwachsinn
-            $message = preg_replace('/\s{2,}/', ' ', $message);
+            $message = preg_replace('/[^\S\n]{2,}/', ' ', $message);
+            
+            // Leerzeichen am Zeilenende entfernen
+            $message = preg_replace('/[^\S\n]+?\n/', "\n", $message);
             $sql = "INSERT INTO log (origin, severity, message) VALUES
                 (\"{$db->escape_string($origin)}\",
                 \"{$db->escape_string($severity)}\",
--- tpl/log.tpl.php
+++ tpl/log.tpl.php
@@ -12,7 +12,7 @@
                         <td>'.$log['time'].'</td>
                         <td>'.$log['severity'].'</td>
                         <td>'.$log['origin'].'</td>
-                        <td>'.$log['message'].'</td>
+                        <td>'.nl2br($log['message']).'</td>
                     </tr>';
     }
     $v['content'] .= '</table>';
