d4046b3fd256eecd82b3a325614589ec5d8badeb
Jens Gutermuth <jens@gutermuth-online.de>
2010-08-23 20:42:18 +0200

    config_default.php aktualisiert und an neues Format angepasst

--- config_default.php
+++ config_default.php
@@ -1,12 +1,14 @@
 <?
+require_once(dirname(__FILE__)."/helper/ConfigHandler.class.php");
 function get_config_from_config_file() {
-    $config['db']['host'] = "localhost";
-    $config['db']['username'] = "squidstats";
-    $config['db']['password'] = "squidstats";
-    $config['db']['dbname'] = "squidstats";
-    $config['log']['min_severity'] = 0;
-    $config['basepath'] = dirname(__FILE__);
-    $config['tpl']['template_dir'] = "tpl";
-    return $config;
+  $config = array();  $config["db"] =  array (
+  'host' => 'localhost',
+  'username' => 'squidstats',
+  'password' => 'squidstats',
+  'dbname' => 'squidstats',
+); 
+  $config["log"] =  'log'; 
+  $config["tpl"] =  'tpl'; 
+  $config["basepath"] = dirname(__FILE__);
 }
 ?>
