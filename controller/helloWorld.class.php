9bd537c00e44939687a42412404aafd4cba85096
Jens Gutermuth <jens@gutermuth-online.de>
2010-08-23 20:31:32 +0200

    dynamischer Controller-Aufruf von web/index.php.
    URL: PROJECTROOT/web/index.php/CLASSNAME/FUNCTION/ARGS..
    classname und function müssen alphanumerisch sein

---
+++ controller/helloWorld.class.php
@@ -0,0 +1,8 @@
+<?
+    class helloWorld {
+        public function index($args) {
+            nl2br(print_r($args, true));
+            echo "hello World!";
+        }
+    }
+?>
--- web/index.php
+++ web/index.php
@@ -3,21 +3,60 @@
     require_once(dirname(__FILE__)."/../helper/ConfigHandler.class.php");
     require_once(dirname(__FILE__)."/../helper/LogHandler.class.php");
     require_once(dirname(__FILE__)."/../helper/TemplateHandler.class.php");
-    
     $config = ConfigHandler::getInstance();
-    $config->log = array('min_severity' => 0);
-    $config->tpl = array('template_dir' => "tpl");
-    $config->log['min_severity'] = 20;
-//    $config->tpl['template_dir'] = "tpl";
-    function test_log() {
-        $log = new LogHandler();
-        $log->log(LogHandler::SEVERITY_DEBUG, "User hat Seite index.php aufgerufen");
+    
+    // Argumente aus URL hohlen
+    
+    $urlData = explode("/",$_SERVER['REQUEST_URI']);
+    for ($i = 0; $i < count($urlData); $i++) {
+        if ($urlData[$i] == basename(__FILE__)) {
+            $urlData = array_slice($urlData, $i+1, count($urlData));
+        }
     }
-        
-    test_log();
-    $tpl = new TemplateHandler();
-    $vars = array();
-    $vars["test"] = "Lorem Ipsum";
-    $vars["test2"] = "Ne, ne ne";
-    $tpl->render("index", $vars);
+    function isEmptyString($var) { return $var !== ""; }
+    $urlData = array_filter($urlData, "isEmptyString"); // entfernt leere Einträge
+
+    if (isset($urlData[0])) {
+        $classname = $urlData[0];
+    } else {
+        $classname = $config->routing['defaultClass'];
+    }
+    if (isset($urlData[1])) {
+        $function = $urlData[1];
+    } else {
+        $function = $config->routing['defaultFunction'];
+    }
+    $args = array_slice($urlData, 2, count($urlData));
+    if (!ctype_alnum($classname) && !ctype_alnum($function)) {
+        throw new Exception("Illegale Zeichen in Klasse oder Funktion");
+    }
+
+
+    function strEndsWith($str, $end)
+    {
+        $len = strlen($end);
+        $strend = substr($str, strlen($str) - $len);
+        return $strend == $end;
+    }
+
+    // nach Klasse suchen und diese laden
+    $dir = $config->basepath.'/controller';
+    if($dirhandle = dir($dir)) {
+        while (false !== ($file = $dirhandle->read())) {
+            if(!is_dir($dir.'/'.$file)) {
+                if (strEndsWith($file, '.class.php') &&
+                (strtolower($file) == strtolower($classname.'.class.php'))) {
+                    require_once($dir.'/'.$file);
+                    // Den aus dem Dateinamen nehmen, da der richtige Groß/Kleinschreibung hat
+                    $classname = substr($file, 0, strlen($file)-strlen('.class.php'));
+                    $obj = new $classname;
+                    if (is_callable(array($obj, $function))) {
+                        call_user_func(array($obj, $function), $args);
+                    }
+                }
+            }
+        }
+        $dirhandle->close();
+    }
+    
 ?>
