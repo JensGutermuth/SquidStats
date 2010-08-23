2893efb1713c7f9b061a1213d3512a78597d290f
Jens Gutermuth <jens@gutermuth-online.de>
2010-08-23 22:03:03 +0200

    404 sendet nun auch wirklich den Statuscode 404

--- controller/Error404.class.php
+++ controller/Error404.class.php
@@ -5,8 +5,8 @@ require_once(dirname(__FILE__).'/../helper/TemplateHandler.class.php');
 class Error404 extends BaseController {
     public function index($args) {
         $tpl = new TemplateHandler();
-//        header("HTTP/1.1 404 Not Found");
-//        header("Status: 404 Not Found");
+        header("HTTP/1.1 404 Not Found");
+        header("Status: 404 Not Found");
         $tpl->render('404', array());
     }
 }
