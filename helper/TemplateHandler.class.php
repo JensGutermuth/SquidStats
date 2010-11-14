2b2bfb57a7c97a75ef07ed5afeca26e2e27dbb29
Jens Gutermuth <jens@jens-eee.(none)>
2010-11-14 22:11:35 +0100

    FIlterfunktionen im TemplateHandler

--- helper/TemplateHandler.class.php
+++ helper/TemplateHandler.class.php
@@ -1,9 +1,11 @@
 <?
 
 require_once(dirname(__FILE__)."/ConfigHandler.class.php");
+require_once(dirname(__FILE__)."/FilterHandler.class.php");
 
-class TemplateHandler
-{
+class TemplateHandler {
+	const FILTER_SLOT_FINAL = 'template_final';
+	const FILTER_SLOT_VARS = 'template_vars';
     private function get_include_contents($filename) {
         if (is_file($filename)) {
             ob_start();
@@ -19,12 +21,21 @@ class TemplateHandler
         $config = ConfigHandler::getInstance();
         $tpl_filename = $config['basepath']."/".$config['tpl']['template_dir'].'/'.$template.'.tpl.php';
         if (is_file($tpl_filename)) {
+            $filter = FilterHandler::getInstance();
             global $v;
             global $t;
-            $v = $vars; // Variablen / Daten fürs Template
+            
+			// Variablen / Daten fürs Template
+            $v = $filter->useSlot(self::FILTER_SLOT_VARS, $vars);
             $c = $config; // Konfiguration
             $t = $this; // Hilfsfunktionen
+            
+            ob_start();
             require ($tpl_filename);
+            $site = ob_get_contents();
+            ob_end_clean();
+            echo $filter->useSlot(self::FILTER_SLOT_FINAL, $site);
+            
             unset($v);
             unset($t);
         }
